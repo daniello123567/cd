ac23fd268776ca4c1404c0da609337cc2656d2a8
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2025-06-24 03:05:41 +0100

    hh

--- app/components/Newpost.tsx
+++ app/components/Newpost.tsx
@@ -241,7 +241,7 @@ const onEmojiClick = (emojiData:{emoji:string}) => {
         </div>
              {error&&<p className={`${ChirpBold.className} text-red-600`}>{error}</p>}
 
-         {publicUrl&&<div className='relative bg-yellow-500 mx-auto mb-[1em] mt-[.5em] max-w-[328px] max-h-[328px] rounded-[1em] w-full h-fit'>
+         {publicUrl&&<div className='relative bg-yellow-500 mx-auto mb-[1em] mt-[.5em] max-w-[328px] overflow-hidden max-h-[328px] rounded-[1em] w-full h-fit'>
 <img src={publicUrl} className='w-full h-auto object-cover' alt='' />
    <svg onClick={handleDelete} width={18.75} height={18.75}  className='absolute cursor-pointer top-[1em] right-[1em]' fill='#f4212e' viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M16 6V4.5C16 3.12 14.88 2 13.5 2h-3C9.11 2 8 3.12 8 4.5V6H3v2h1.06l.81 11.21C4.98 20.78 6.28 22 7.86 22h8.27c1.58 0 2.88-1.22 3-2.79L19.93 8H21V6h-5zm-6-1.5c0-.28.22-.5.5-.5h3c.27 0 .5.22.5.5V6h-4V4.5zm7.13 14.57c-.04.52-.47.93-1 .93H7.86c-.53 0-.96-.41-1-.93L6.07 8h11.85l-.79 11.07zM9 17v-6h2v6H9zm4 0v-6h2v6h-2z"></path></g></svg>
 
